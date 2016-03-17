f32b6a329b652012526a13f37177105834ac9693
Andrew Lee <andrew@improbable.io>
2016-03-17 14:35:48 +0000

    Upgrade to 4.1.0

--- workers/gsim/src/main/scala/improbable/natures/BotNature.scala
+++ workers/gsim/src/main/scala/improbable/natures/BotNature.scala
@@ -1,21 +1,24 @@
 package improbable.natures
 
-import improbable.corelib.natures.rigidbody.RigidbodyComposedTransformNature
-import improbable.corelib.natures.{NatureApplication, NatureDescription}
+import improbable.corelib.natures.{BaseNature, NatureApplication, NatureDescription}
+import improbable.corelibrary.rigidbody.RigidbodyNature
+import improbable.corelibrary.transforms.TransformNature
 import improbable.math.Coordinates
 import improbable.papi.entity.behaviour.EntityBehaviourDescriptor
 import improbable.util.EntityPrefabs.BOT
 
 object BotNature extends NatureDescription {
 
-  override def dependencies = Set[NatureDescription](RigidbodyComposedTransformNature, ColoredNature)
+  override def dependencies = Set[NatureDescription](BaseNature, TransformNature, RigidbodyNature, ColoredNature)
 
   override def activeBehaviours: Set[EntityBehaviourDescriptor] = Set.empty
 
   def apply(initialPosition: Coordinates): NatureApplication = {
     application(
       natures = Seq(
-        RigidbodyComposedTransformNature(entityPrefab = BOT, initialPosition = initialPosition, drag = 0.2f),
+        BaseNature(entityPrefab = BOT),
+        TransformNature(initialPosition.toVector3d),
+        RigidbodyNature(drag = 0.2f),
         ColoredNature(color = java.awt.Color.white))
     )
   }
--- workers/gsim/src/main/scala/improbable/natures/TreeNature.scala
+++ workers/gsim/src/main/scala/improbable/natures/TreeNature.scala
@@ -1,21 +1,22 @@
 package improbable.natures
 
-import improbable.corelib.natures.base.BaseComposedTransformNature
-import improbable.corelib.natures.{NatureApplication, NatureDescription}
+import improbable.corelib.natures.{BaseNature, NatureApplication, NatureDescription}
+import improbable.corelibrary.transforms.TransformNature
 import improbable.math.Coordinates
 import improbable.papi.entity.behaviour.EntityBehaviourDescriptor
 import improbable.util.EntityPrefabs.TREE
 
 object TreeNature extends NatureDescription {
 
-  override def dependencies: Set[NatureDescription] = Set(BaseComposedTransformNature)
+  override def dependencies: Set[NatureDescription] = Set(BaseNature, TransformNature)
 
   override def activeBehaviours: Set[EntityBehaviourDescriptor] = Set.empty
 
   def apply(initialPosition: Coordinates): NatureApplication = {
     application(
       natures = Seq(
-        BaseComposedTransformNature(entityPrefab = TREE, initialPosition = initialPosition)
+        BaseNature(entityPrefab = TREE),
+        TransformNature(initialPosition.toVector3d)
       )
     )
   }
