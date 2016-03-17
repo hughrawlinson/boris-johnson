55f2c142a7414a3dac5d7cee0c685e98833e7748
Andrew Lee <andrew@improbable.io>
2016-03-17 17:14:21 +0000

    Added RigidbodyNature to TreeNature

--- workers/gsim/src/main/scala/improbable/natures/TreeNature.scala
+++ workers/gsim/src/main/scala/improbable/natures/TreeNature.scala
@@ -1,6 +1,7 @@
 package improbable.natures
 
 import improbable.corelib.natures.{BaseNature, NatureApplication, NatureDescription}
+import improbable.corelibrary.rigidbody.RigidbodyNature
 import improbable.corelibrary.transforms.TransformNature
 import improbable.math.Coordinates
 import improbable.papi.entity.behaviour.EntityBehaviourDescriptor
@@ -8,7 +9,7 @@ import improbable.util.EntityPrefabs.TREE
 
 object TreeNature extends NatureDescription {
 
-  override def dependencies: Set[NatureDescription] = Set(BaseNature, TransformNature)
+  override def dependencies: Set[NatureDescription] = Set(BaseNature, TransformNature, RigidbodyNature)
 
   override def activeBehaviours: Set[EntityBehaviourDescriptor] = Set.empty
 
@@ -16,7 +17,8 @@ object TreeNature extends NatureDescription {
     application(
       natures = Seq(
         BaseNature(entityPrefab = TREE),
-        TransformNature(initialPosition.toVector3d)
+        TransformNature(initialPosition.toVector3d),
+        RigidbodyNature()
       )
     )
   }
